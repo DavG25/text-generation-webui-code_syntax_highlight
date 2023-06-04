8c76083b03297e794a17bf062372757804774557
DavG25 <git@davg25.com>
2023-06-04 04:50:51 +0200

    Add comments

--- assets/highlightjs-copy.js
+++ assets/highlightjs-copy.js
@@ -11,25 +11,32 @@ class CopyButtonPlugin {
   }
 
   'after:highlightElement'({ el, text }) {
+    // Create copy button
     const button = Object.assign(document.createElement('button'), {
       innerHTML: this.locales[this.lang]?.[0] || 'Copy',
       className: 'hljs-copy-button',
     });
     button.dataset.copied = false;
 
+    // Create reference to <pre> tag
+    const wrapper = el.parentElement;
+
     // Check if code block height is not enough for the copy button to fit, then add class to fix
-    if (el.parentElement.offsetHeight < 42) {
-      el.parentElement.classList.add('hljs-copy-narrow-wrapper');
+    if (wrapper.offsetHeight < 42) {
+      wrapper.classList.add('hljs-copy-narrow-wrapper');
       // Calculate and apply the top margin to align
       // copy button in the middle of the narrow code block
-      let topMargin = Math.floor((el.parentElement.offsetHeight - 32) / 2);
+      let topMargin = Math.floor((wrapper.offsetHeight - 32) / 2);
       if (topMargin < 0) topMargin = 0;
       else if (topMargin > 5) topMargin = 5;
       button.style.setProperty('margin-top', `${topMargin}px`, 'important');
     }
-    el.parentElement.classList.add('hljs-copy-wrapper');
-    el.parentElement.appendChild(button);
 
+    // Add button in the DOM
+    wrapper.classList.add('hljs-copy-wrapper');
+    wrapper.appendChild(button);
+
+    // Handle copy to clipboard on button click
     button.onclick = () => {
       if (!navigator.clipboard) {
         // Some browsers (for example on Android) require an HTTPS connection
@@ -39,27 +46,31 @@ class CopyButtonPlugin {
         return;
       }
 
+      // Get text from code block
       let newText = text;
       if (this.hook && typeof hook === 'function') {
         newText = this.hook(text, el) || text;
       }
 
+      // Write text into the clipboard
       navigator.clipboard.writeText(newText).then(() => {
+        // Update button status
         button.innerHTML = this.locales[this.lang]?.[1] || 'Copied!';
         button.dataset.copied = true;
         button.tabIndex = -1;
         button.disabled = true;
         button.style.pointerEvents = 'none';
 
+        // Alert for screen readers
         let alert = Object.assign(document.createElement('div'), {
           role: 'status',
           className: 'hljs-copy-alert',
           innerHTML: this.locales[this.lang]?.[2] || 'Copied to clipboard',
         });
         alert.setAttribute('style', 'clip: rect(0 0 0 0) !important; clip-path: inset(50%) !important; height: 1px !important; overflow: hidden !important; position: absolute !important; white-space: nowrap !important; width: 1px !important;');
-        el.parentElement.appendChild(alert);
+        wrapper.appendChild(alert);
 
-        // TODO: Reset previous timeout on new button press
+        // Revert button status
         setTimeout(() => {
           // Remove focus from button so it hides again
           button.blur();
@@ -68,7 +79,7 @@ class CopyButtonPlugin {
           button.removeAttribute('tabindex');
           button.disabled = false;
           button.style.pointerEvents = null;
-          el.parentElement.removeChild(alert);
+          wrapper.removeChild(alert);
           alert = null;
         }, 2000);
       }).then(() => {
